70698726eba1e4f00a799b72e148b329168ee7a6
AccessForensics <marissa.wright@accessforensics.io>
2026-03-04 22:46:42 -0500

    test: isolate Jest from Playwright specs, add minimal smoke test
    * Fix smoke-test: remove duplicates and enforce contextFactory baselines
    
    * test: isolate jest from playwright specs, add smoke test
    
    ---------

---
+++ jest.config.cjs
@@ -0,0 +1,17 @@
+﻿/** @type {import("jest").Config} */
+module.exports = {
+  testEnvironment: "node",
+
+  // Run only Jest tests
+  testMatch: [
+    "**/__tests__/**/*.test.js",
+    "**/tests/**/*.test.js"
+  ],
+
+  // Do NOT let Jest try to run Playwright Test or Node's test runner suites
+  testPathIgnorePatterns: [
+    "/node_modules/",
+    "\\.spec\\.js$",
+    "template3_preconditions\\.test\\.js$"
+  ],
+};
